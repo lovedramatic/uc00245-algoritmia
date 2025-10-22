a47aafcc783137c8b13756aee4f2036b5552f19b
maria <mimmpmadeira@gmail.com>
2025-10-22 19:45:08 +0100

    1º commit, criação components

--- 00245/src/app/app.ts
+++ 00245/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('00245');
+  //protected readonly title = signal('00245');
 }
